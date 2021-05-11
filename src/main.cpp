8c6f34f84808515d952c122fd993beb5b83a7911
Chuck Rabb <chuck.rabb@gmail.com>
2021-05-11 12:16:49 -0400

    Still moving to platformio

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,41 @@
 #include <Arduino.h>
+#include "singleton_t.h"
+#include "heartbeat.h"
+#include "simon_consts.h"
+#include "panel_list.h"
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
 #endif 
 
+using namespace simon;
+
+
 void setup()
 {
-    pinMode( LED_BUILTIN, OUTPUT );
+    init_singletons();
+    neopixel& pixels = singleton_t< neopixel >::instance();
+    pixels.begin();
+    pixels.show();
+
+    delay( 1000 );
+
 }
 
+boolean first_time = true;
 void loop()
 {
-    digitalWrite( LED_BUILTIN, HIGH );
 
-    delay( 500 );
-
-    digitalWrite( LED_BUILTIN, LOW );
-
-    delay( 500 );
+    heartbeat&  hb = singleton_t< heartbeat >::instance();
+    panel_list& pl = singleton_t< panel_list >::instance();
+    
+    hb.beat();
+    pl.tick();
 }
 
+void init_singletons()
+{
+    singleton_t< heartbeat  >( new heartbeat( HEARTBEAT_PIN, HEARTBEAT_DURATION_OFF, HEARTBEAT_DURATION_ON ) );
+    singleton_t< neopixel   >( new neopixel( NUM_NEOPIXELS, NEOPIXEL_PIN, NEO_RGB + NEO_KHZ800 ) );
+    singleton_t< panel_list >( new panel_list() );
+}
