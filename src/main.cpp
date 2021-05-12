94c7d48af46d215f87cb206105422fcaea515957
Chuck Rabb <chuck.rabb@gmail.com>
2021-05-11 21:14:54 -0400

    This is compiling and running but there are issues

--- src/main.cpp
+++ src/main.cpp
@@ -13,29 +13,63 @@ using namespace simon;
 void init_singletons()
 {
     singleton_t< heartbeat  >( new heartbeat( HEARTBEAT_PIN, HEARTBEAT_DURATION_OFF, HEARTBEAT_DURATION_ON ) );
-    singleton_t< neopixel   >( new neopixel( NUM_NEOPIXELS, NEOPIXEL_PIN, NEO_RGB + NEO_KHZ800 ) );
+    //singleton_t< neopixel   >( new neopixel( NUM_NEOPIXELS, NEOPIXEL_PIN, NEO_RGB + NEO_KHZ800 ) );
     singleton_t< panel_list >( new panel_list() );
 }
 
 void setup()
 {
+
+    Serial.begin( 115200 );
+
+    
     init_singletons();
+
+    /*
     neopixel& pixels = singleton_t< neopixel >::instance();
     pixels.begin();
     pixels.show();
+    */
 
     delay( 1000 );
 
+    Serial.println( "Hello" );
+
+    pinMode( LED_BUILTIN, OUTPUT );
 }
+
+long last_touch = millis();
+bool is_on = false;
+void cheap_heartbeat()
+{
+    if ( ( millis() - last_touch ) > 1000 )
+    {
+        if ( is_on )
+        {
+            // Turn off
+            //
+            digitalWrite( LED_BUILTIN, LOW );
+            is_on = false;
+            Serial.println( "Off" );
+        }
+        else
+        {
+            digitalWrite( LED_BUILTIN, HIGH );
+            is_on = true;
+            Serial.println( "On" );
+        }
+
+        last_touch = millis();
+    }
+}
+
 
 void loop()
 {
-
-    heartbeat&  hb = singleton_t< heartbeat >::instance();
-    panel_list& pl = singleton_t< panel_list >::instance();
-    
+    //cheap_heartbeat();
+    heartbeat& hb = singleton_t< heartbeat >::instance();
     hb.beat();
-    pl.tick();
-}
+
+} 
 
 
