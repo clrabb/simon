6c12126bd0ecfb91383c64f37ac67181b74489c5
Chuck Rabb <chuck.rabb@gmail.com>
2021-05-18 18:30:59 -0400

    Commit before renaming button_panel to panel

---
+++ lib/simon_core/include/panel_activation_token.h
@@ -0,0 +1,13 @@
+#ifndef PANEL_ACTIVATION_TOKEN
+#define PANEL_ACTIVATION_TOKEN
+
+namespace simon
+{
+    class panel_activation_token
+    {
+
+    };
+}
+
+
+#endif // PANEL_ACTIVATION_TOKEN
---
+++ lib/simon_core/include/panel_state.h
@@ -0,0 +1,4 @@
+#ifndef PANEL_STATE_H
+#define PANEL_STATE_H
+
+#endif // PANEL_STATE_H
